6e008556680e937de74e5b376315d485c0bf5ad4
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-07-20 21:04:46 -0300

    feat: inserts

---
+++ inserts.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuario(cpf, nome, genero, telefone, data_nasc, endereco, email, tipo_sanguineo) VALUES
+('345.678.910-12', 'Priscila Holanda', 'F', '(85)9905-4321', '2007-01-01', 'Av. Beira Mar, 123, Meireles, Fortaleza, CE', 'priscila@email.com', 'O+'),
+('902.091.994-47', 'Kaio Silva', 'M', '(88)9834-1209', '2009-03-21', 'Rua Coronel Diogo, 07, Centro, Sobral, CE', 'kaio@email.com', 'AB-'),
+('055.095.334-87', 'Vitoria Rabelo', 'F', '(88)9384-3567', '2001-05-30', 'Rua São Pedro, 30, Centro, Juazeiro do Norte, CE', 'vivi@email.com', 'A-'),
+('404.345.234-22', 'Mariana Arcelino', 'F', '(85)9233-0372', '2010-02-20', 'Rua dos Pacajús, 43, Aldeota, Fortaleza, CE', 'mari@email.com', 'A+'),
+('537.998.808-23', 'Marcelo Costa', 'M', '(88)9007-4296', '2012-12-10', 'Av. José de Freitas, 101, Centro, Quixadá, CE', 'costamarcelo@email.com', 'A+'),
+('889.435.280-10', 'Mateus Miranda', 'M', '(85)9789-4241', '2003-11-13', 'Rua Professor Gomes, 111, Centro, Canindé, CE', 'mat@email.com', 'O+'),
+('008.231.712-29', 'Marian Oliveira', 'F', '(85)9884-2143', '2004-07-14', 'Av. Dom Luís, 1002, Meireles, Fortaleza, CE', 'marianoliveira@email.com', 'AB+'),
+('999.415.556-78', 'Renata Souza', 'F', '(85)9334-2178', '2002-04-28', 'Rua Pereira Valente, 505, José Bonifácio, Fortaleza, CE', 'renata@email.com', 'AB-'),
+('301.678.063-80', 'Orlando Macena', 'M', '(85)9020-3435', '2001-06-17', 'Rua Major Facundo, 302, Centro, Iguatu, CE', 'orlando@email.com', 'B-'),
+('304.202.304-90', 'Monica Dantas', 'F', '(85)9022-8835', '2005-09-07', 'Rua Castro e Silva, 202, Jacarecanga, Fortaleza, CE', 'monica@email.com', 'A+');
+
+
